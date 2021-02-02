f50fd04c881e4c53a24e2cdd25e96915b49c3ec3
Pournimasambare2307 <73574721+Pournimasambare2307@users.noreply.github.com>
2021-02-02 13:30:27 +0530

    Update AdminController.java

--- controllers/AdminController.java
+++ controllers/AdminController.java
@@ -1,5 +1,7 @@
 package com.demo.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,42 +15,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.demo.model.Police;
+
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/efir")
 public class AdminController {
 	@GetMapping("/police")
 	 public ResponseEntity<List<Police>> getAllPolice(@RequestParam(required = false) String title) {
+		return null;
 		    
 		  }
 	 @GetMapping("/police/{id}")
 	  public ResponseEntity<Police> getPoliceById(@PathVariable("id") long id) {
+		return null;
 	   
 	  }
 
 	  @PostMapping("/police")
 	  public ResponseEntity<Police> addPolice(@RequestBody Police police) {
+		return null;
 	    
 	  }
 
 	  @PutMapping("/police/{id}")
 	  public ResponseEntity<Police> updatePolice(@PathVariable("id") long id, @RequestBody Police police) {
+		return null;
 	    
 	  }
 
 	  @DeleteMapping("/police/{id}")
 	  public ResponseEntity<HttpStatus> deletePolice(@PathVariable("id") long id) {
+		return null;
 	   
 	  }
 
 	  @DeleteMapping("/tutorials")
 	  public ResponseEntity<HttpStatus> deleteAllPolice() {
+		return null;
 	   
 	  }
 
 	  @GetMapping("/tutorials/published")
 	  public ResponseEntity<List<Police>> findByFIR() {
+		return null;
 	   
 	  }
 	}
-}
+
+
